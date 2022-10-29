4ccfd606d5fe3d83824e507c23bd607af5b2f77b
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-10-29 09:51:22 +0200

    Aktualizacja pliku w repozytorium.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # This is a sample Python script.
+# Druga linijka komentarza
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
